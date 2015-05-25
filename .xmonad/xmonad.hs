1581e1feff15908f8c66307b770f085172f3e424
Albert Faust <albertfaust1504@gmail.com>
2015-05-25 23:27:35 +0100

    Create xmonad.hs
    Config file for 'xmonad' written in Haskell.

---
+++ .xmonad/xmonad.hs
@@ -0,0 +1,15 @@
+main = do
+    xmproc <- spawnPipe "/usr/bin/xmobar ~/.xmobarrc"
+    xmonad $ defaultConfig
+        { manageHook = manageDocks <+> manageHook defaultConfig
+        , layoutHook = avoidStruts  $  layoutHook defaultConfig
+        , logHook = dynamicLogWithPP xmobarPP
+                        { ppOutput = hPutStrLn xmproc
+                        , ppTitle = xmobarColor "green" "" . shorten 50
+                        }
+        , modMask = mod4Mask
+        } 'additionalKeys'
+        [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
+        , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
+        , ((0, xK_Print), spawn "scrot")
+        ]
