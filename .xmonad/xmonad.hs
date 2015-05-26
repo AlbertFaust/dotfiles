0495e84117942d8b190d763719f45c460bd6c71a
Albert Faust <albertfaust1504@gmail.com>
2015-05-26 12:23:07 +0100

    Update xmonad.hs
    Fixed typos.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -8,7 +8,7 @@ main = do
                         , ppTitle = xmobarColor "green" "" . shorten 50
                         }
         , modMask = mod4Mask
-        } 'additionalKeys'
+        } `additionalKeys`
         [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
         , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
         , ((0, xK_Print), spawn "scrot")
